2eed0a811e6f0eeedf4438f61cccf6c9d9c09111
Rakibul Hasan <rakibul.rupom2001@gmail.com>
2022-04-10 13:23:35 +0600

    adding bg and design UI

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const loadWeather = ()=>{
+    document.getElementById("display-div").innerHTML='';
     const searchBox = document.getElementById("search-field");
     const city = searchBox.value;
     searchBox.value='';
 
     fetch(`https://api.weatherapi.com/v1/current.json?key=3ed5346737c14e0bad1193253220904&q=${city}`)
     .then(res=>res.json())
-    .then(data=>displayDetails(data));
+    .then(data=>displayDetails(data))
+    .catch(error=>showError(city))
 }
 
 const displayDetails = (data)=>{
     const displayDiv = document.getElementById("display-div");
     const newDiv = document.createElement("div");
     newDiv.innerHTML=`
-        <h3>${data.location.name}</h3>
+        <h1>${data.location.name}</h1>
         <h2>${data.location.country}</h2>
         <img src=${data.current.condition.icon} alt="">
-        <p>${data.current.condition.text}</p>
-        <p>${data.current.humidity}</p>
-        <h4>Temperature: ${data.current.temp_c} C</h4>
-        <small>${data.current.last_updated}</small>
+        <h3><strong>${data.current.condition.text}</strong></h3>
+        <p>Humidity: <strong>${data.current.humidity}</strong></p>
+        <h4>Temperature: ${data.current.temp_c} °C</h4>
+        <small>Last updated: <i>${data.current.last_updated}</i></small>
     `;
     displayDiv.appendChild(newDiv);
+}
+
+const showError = (city) =>{
+    const errorDiv = document.getElementById("display-error");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML=`
+        <h4 class="text-danger">Warning!!! Can't find the info of ${city}.</h4>
+        <h3>Please insert a valid city name...</h3>
+    `;
+    errorDiv.appendChild(newDiv);
 }
