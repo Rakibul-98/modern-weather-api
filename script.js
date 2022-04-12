70a531071fbe7a167da76625827658501bb7ebf7
Rakibul Hasan <rakibul.rupom2001@gmail.com>
2022-04-12 20:08:05 +0600

    website completed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const loadWeather = ()=>{
+    showSpinner("block");
     document.getElementById("display-div").innerHTML='';
     document.getElementById("display-error").innerHTML='';
     const searchBox = document.getElementById("search-field");
@@ -12,6 +13,7 @@ const loadWeather = ()=>{
 }
 
 const displayDetails = (data)=>{
+    showSpinner("none");
     const displayDiv = document.getElementById("display-div");
     const newDiv = document.createElement("div");
     newDiv.innerHTML=`
@@ -20,25 +22,32 @@ const displayDetails = (data)=>{
         <img src=${data.current.condition.icon} alt="">
         <h3><strong>${data.current.condition.text}</strong></h3>
         <p>Humidity: <strong>${data.current.humidity}</strong></p>
-        <h4>Temperature: ${data.current.temp_c} °C</h4>
-        <small>Last updated: <i>${data.current.last_updated}</i></small>
+        <h4>Temperature: ${data.current.temp_c}° C</h4>
+        <small class="text-light">Last updated: <i>${data.current.last_updated}</i></small>
     `;
     displayDiv.appendChild(newDiv);
 }
 
+const showSpinner = type =>{
+    document.getElementById("spinner").style.display=type;
+}
+
 const showError = (city) =>{
+    showSpinner("none");
     const errorDiv = document.getElementById("display-error");
     const newDiv = document.createElement("div");
     if(city.length == 0 || city.indexOf(' ') >= 0){
         newDiv.innerHTML=`
+            <img class="img-fluid" src="images/error-img.png" alt="">
             <h4 class="text-danger">ERROR!!! Search box can't be empty.</h4>
-            <h3>Please insert a valid city name...</h3>
+            <h3 class="text-light">Please insert a valid city name...</h3>
         `;
     }
     else if (city.length != 0) {
         newDiv.innerHTML=`
-            <h4 class="text-danger">ERROR!!! Can't find the info of ${city}</h4>
-            <h3>Please insert a valid city name...</h3>
+            <img class="img-fluid" src="images/error-img.png" alt="">
+            <h4 class="text-danger">ERROR!!! Can't find the info of "${city}"</h4>
+            <h3 class="text-light">Please insert a valid city name...</h3>
         `;
     } 
     errorDiv.appendChild(newDiv);
